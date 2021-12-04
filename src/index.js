60d309c5cded0aec048a01061fddd3b1d76ad4d1
yiyi123-6 <yiyitan076@qq.com>
2021-12-04 17:43:38 -0500

    Update the Search Engine Page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
+import { BrowserRouter as Router, Route, Routes, HashRouter, Link, Redirect } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import './index.css';
+import Home from './components/Home';
+import Search from './components/Search';
+import AcademicTable from './components/Tables/AcademicTable';
+import ArticleTable from './components/Tables/ArticleTable';
+import ContactTable from './components/Tables/ContactTable';
+import DetailTable from './components/Tables/DetailTable';
+import EmployTable from './components/Tables/EmployTable';
+import NotiTable from './components/Tables/NotiTable';
+
+ReactDOM.render((
+<Router basename="/">
+    <Routes>
+        <Route path = "/" element={ <Home/>} />
+        <Route path = "/search" element={ <Search/>} />
+        <Route path = "/academictable" element={ <AcademicTable/>} />
+        <Route path = "/articletable" element={ <ArticleTable/>} />
+        <Route path = "/contacttable" element={ <ContactTable/>} />
+        <Route path = "/detailtable" element={ <DetailTable/>} />
+        <Route path = "/employtable" element={ <EmployTable/>} />
+        <Route path = "/notitable" element={ <NotiTable/>} />
+      </Routes>
+  </Router>),
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
