37815e2ebd9074296d8277c675952952e3dd65bd
yiyi123-6 <yiyitan076@qq.com>
2021-12-04 09:23:24 -0500

    Empty frame

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
-import logo from './logo.svg';
-import { Button } from 'antd';
 import './App.css';
+import React, { useState } from 'react';
+import { Table } from 'antd';
+import { Layout, Menu, Breadcrumb, Typography} from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const { Title } = Typography
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary">Button</Button>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>
+              Dashboard
+            </Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>
+              Search Engine
+            </Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">User1</Menu.Item>
+              <Menu.Item key="4">User2</Menu.Item>
+              <Menu.Item key="5">User3</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>
+              Files
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Manager View</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              <Title>
+                Dashboard
+              </Title>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>CSSA 会友部</Footer>
+        </Layout>
+      </Layout>
   );
 }
 
+//// Pages
+// import Search from "./pages"
+
+
+// function render(){
+//   return <Router>
+//   <Route path="/" component={Search}/>
+//     </Router>
+// }
+
 export default App;
+// export default render;
